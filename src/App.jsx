0d3cddcabc341f625fffd1db0d523011effaf814
Daewoong <kodw.kor@gmail.com>
2024-08-06 23:14:00 +0900

    example wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import "./App.css";
+
 function App() {
 
   return (
-    <>
-    </>
+    <div className="flexbox-container">
+      <div className="name-container">
+        <div className="round-container flexbox-item-1">
+          A
+        </div>
+        <div>
+          Alice
+        </div>
+      </div>
+      <div className="round-container flexbox-item-2">
+        B
+      </div>
+      <div className="round-container flexbox-item-3">
+        C
+      </div>
+    </div>
   )
 }
 
